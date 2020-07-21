5cb49f2254d83fddfae7d09d3cf5cca3fd0c4173
Vhieu <vanhieuu99@gmail.com>
2020-07-21 21:56:52 +0700

    baitap array

--- Session5_Loop/Loop.js
+++ Session5_Loop/Loop.js
@@ -1,150 +1,150 @@
-// let chon = parseInt(prompt("Chọn bài tập"));
-//     switch (chon){
-//         case 1 : 
-//                 bai1();
-//                 break;
-//         case 2:
-//                 bai2();
-//                 break;
-//         case 3:
-//                 bai3();
-//                 break;
-//         case 4:
-//                 bai4();
-//                 break;
-//         case 5:
-//                 bai5();
-//                 break;
-//         case 6:
-//                 bai6();
-//                 break;
-//         case 7:
-//                 bai7();
-//                 break;
-//         case 8: 
-//                 bai8();
-//                 break;
-//         case 9:
-//                 bai9();
-//                 break;
-//         case 10:
-//                 bai10();
-//                 break;
-//         case 11:
-//                 bai11();
-//             default:
+let chon = parseInt(prompt("Chọn bài tập"));
+    switch (chon){
+        case 1 : 
+                bai1();
+                break;
+        case 2:
+                bai2();
+                break;
+        case 3:
+                bai3();
+                break;
+        case 4:
+                bai4();
+                break;
+        case 5:
+                bai5();
+                break;
+        case 6:
+                bai6();
+                break;
+        case 7:
+                bai7();
+                break;
+        case 8: 
+                bai8();
+                break;
+        case 9:
+                bai9();
+                break;
+        case 10:
+                bai10();
+                break;
+        case 11:
+                bai11();
+            default:
             
-//                 break;
-//                  }
+                break;
+                 }
                 
-// function bai1(){
-//     let  dem = parseInt(prompt("Bắt đầu in"));
-//         while (dem <= 100) {
-//             console.log(dem);
-//             dem ++;
-//         }
-// }
-// function bai2(){
-//     let dem = parseInt(prompt("Bắt đầu in từ "));
-//     while ( 0 <= dem && dem <= 100 ){
-//         console.log("In " + dem );  
-//         dem--;
-//     }
-// }
-// function bai3(){
-//      let dem = parseInt(prompt("Bắt đầu in từ "));
-//         for (dem = 0; dem <= 100; dem ++){
-//             if (dem % 2 == 0 ){
-//                 console.log("số chẵn:= " + dem);
-//             }
-//         }
-// }
-// function bai4(){
-//     let dem = parseInt(100);
-//         for (dem = 100;  dem >=0; dem-- ){
-//             if (dem % 2 == 0 ){
-//                 console.log("Số chẵn:= " + dem);
-//             }
-//             else {
-//             console.log("Số lẻ:= " + dem )
-//         }
-//         }
-// }
+function bai1(){
+    let  dem = parseInt(prompt("Bắt đầu in"));
+        while (dem <= 100) {
+            console.log(dem);
+            dem ++;
+        }
+}
+function bai2(){
+    let dem = parseInt(prompt("Bắt đầu in từ "));
+    while ( 0 <= dem && dem <= 100 ){
+        console.log("In " + dem );  
+        dem--;
+    }
+}
+function bai3(){
+     let dem = parseInt(prompt("Bắt đầu in từ "));
+        for (dem = 0; dem <= 100; dem ++){
+            if (dem % 2 == 0 ){
+                console.log("số chẵn:= " + dem);
+            }
+        }
+}
+function bai4(){
+    let dem = parseInt(100);
+        for (dem = 100;  dem >=0; dem-- ){
+            if (dem % 2 == 0 ){
+                console.log("Số chẵn:= " + dem);
+            }
+            else {
+            console.log("Số lẻ:= " + dem )
+        }
+        }
+}
 
-// function bai5() {
-//     var n = parseInt(prompt("Nhập vào n"));
-//     let tong = parseInt(0);
-//     for (let i = 0; i <= n; i++) {
-//         tong = tong + i;
-//     }
-//         console.log("Tổng các phần tử := " + tong);
-// }
-// function bai6(){
-//     var n = parseInt(prompt("Nhập N "));
-//     let tong = parseInt(0);
-//     for (let i = 0 ; i <= n ; i++){
-//         tong = tong + (2*i + 1);
-//     }
-//         console.log("Tổng:= " + tong);
-// }
-// function bai7(){
-//     var n = parseInt(prompt("Nhập N"))
-//     let tong = parseInt(0);
-//         for (let i = 0; i <=n ; i++) {
-//             tong = tong + 2*i;
-//         }
-//             console.log("Tổng:= " + tong);
-// }
-// function bai8(){
-//     var n = parseInt(prompt("Nhập N"));
-//     let tong = parseInt(0);
-//         while (n < 0){
-//            prompt("Nhập lại n " );
-//             }
-//             for (let i = 1; i <=n ; i++) {
-//                 tong += 1/i;
-//             }
-//             console.log("Tổng:= " + tong);
-//         }
-// function bai9() {
-//     var n = parseInt(prompt("Nhập N"));
-//     let tong = parseInt(0);
-//         while (n < 0) {
-//             prompt("Nhập lại N");
-//         }
-//             for (let i = 1;i <= n; i++){
-//                 let gt = i*(i+1)
-//                 tong += 1/gt;
+function bai5() {
+    var n = parseInt(prompt("Nhập vào n"));
+    let tong = parseInt(0);
+    for (let i = 0; i <= n; i++) {
+        tong = tong + i;
+    }
+        console.log("Tổng các phần tử := " + tong);
+}
+function bai6(){
+    var n = parseInt(prompt("Nhập N "));
+    let tong = parseInt(0);
+    for (let i = 0 ; i <= n ; i++){
+        tong = tong + (2*i + 1);
+    }
+        console.log("Tổng:= " + tong);
+}
+function bai7(){
+    var n = parseInt(prompt("Nhập N"))
+    let tong = parseInt(0);
+        for (let i = 0; i <=n ; i++) {
+            tong = tong + 2*i;
+        }
+            console.log("Tổng:= " + tong);
+}
+function bai8(){
+    var n = parseInt(prompt("Nhập N"));
+    let tong = parseInt(0);
+        while (n < 0){
+           prompt("Nhập lại n " );
+            }
+            for (let i = 1; i <=n ; i++) {
+                tong += 1/i;
+            }
+            console.log("Tổng:= " + tong);
+        }
+function bai9() {
+    var n = parseInt(prompt("Nhập N"));
+    let tong = parseInt(0);
+        while (n < 0) {
+            prompt("Nhập lại N");
+        }
+            for (let i = 1;i <= n; i++){
+                let gt = i*(i+1)
+                tong += 1/gt;
                
-//             }
-//             console.log("Tổng:= " + tong);    
-// }
-// function fibonacci() {
-//     let x = 0;
-//     let y = 1;
-//     let z = 0;
-//     return function getNext() {
-//         z = x + y;
-//         x = y;
-//         y = z;
-//         return z;
-//     }
-// }
-// function bai10() {
+            }
+            console.log("Tổng:= " + tong);    
+}
+function fibonacci() {
+    let x = 0;
+    let y = 1;
+    let z = 0;
+    return function getNext() {
+        z = x + y;
+        x = y;
+        y = z;
+        return z;
+    }
+}
+function bai10() {
     
-// var fun = fibonacci();
-// var n = parseInt(prompt("Nhập vào n"));
-// for (let i = 0; i < n; i++) {
-//     console.log(fun());
-//     }
-// }
+var fun = fibonacci();
+var n = parseInt(prompt("Nhập vào n"));
+for (let i = 0; i < n; i++) {
+    console.log(fun());
+    }
+}
 function bai11() {
    
    
     let a = Number(prompt("Nhap vao a"));
     let b = Number(prompt("Nhap vao b "));
-        for( i = 1;i < a; i++ ){    
-            for ( j = 1; j < b ; j++) {
+        for( let i = 1; i < a; i++ ){    
+            for ( let j = 1; j < b ; j++) {
              console.log("*");
             }
                 console.log("\n");
--- Session6_Array/Array.js
+++ Session6_Array/Array.js
@@ -1,12 +1,36 @@
 let arr_empty = []; //Mảng rỗng
-let arr_numbers = [1, 2, 3, 4, 5, 6];
+let arr_numbers = [1, 2, 3, 4, 5, 6, 11];
 let arr_string = ['A' , 'B', 'C ', 'D'];
 let arr_mix = [ 1, 2, 3, 4, "T" , "C", true ] // k dùng 
-console.log(arr_mix);
+console.log(arr_numbers);
 // console.log(arr_numbers[arr_numbers.length - 1]); // lấy giá trị cuối cùng của mảng
-for (let i = 0; i < arr_numbers.length ; i++) {
-    const element = arr_numbers[i];
-        if (element % 2 == 0 ){
-    console.log(element);
-        }
-}
+//     let countOdd = 0;
+//     let isLarger10 = false;
+// for (let i = 0; i < arr_numbers.length ; i++) {
+//     let element = arr_numbers[i];
+//         if (element % 2 === 1 ){
+//             countOdd++
+//         }
+//         if(element  > 10){
+
+//             isLarger1010 = true;
+//             break;
+//         }
+
+//     }
+//         console.log(`có   ${countOdd} số lẻ` );
+//     console.log(isLarger1010);
+    // arr_numbers.push(7,8,9,33);
+    // console.log(arr_numbers);
+    // arr_numbers.unshift(1,5,3,2);
+    // console.log(arr_numbers);
+    // arr_numbers[17] = 10;
+    // console.log(arr_numbers);
+    console.log(arr_numbers);
+    console.log(arr_numbers);
+    let first = arr_numbers.shift();
+    let last = arr_numbers.pop();
+    let del = arr_numbers.splice(1,3)
+    console.log(last);
+    console.log(first);
+    console.log(del);
